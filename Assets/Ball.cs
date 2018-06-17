c6a2cba4ea7857f76330ccf402c89120cc172397
Komstuch <komstuchxbox@gmail.com>
2018-06-17 14:38:22 +0200

    Add ball, pins, camera following the ball and rigidbodies
    Initial speed of the ball is adjustable

---
+++ Assets/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+
+    public Vector3 launchVelocity;
+
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
+        Launch();
+    }
+
+    public void Launch()
+    {
+        rigidBody.velocity = launchVelocity;
+        audioSource.Play();
+    }
+}
---
+++ Assets/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Ball ball;
+
+    private Vector3 offset;
+
+	void Start () {
+        offset =transform.position - ball.transform.position;
+	}
+	
+	void Update () {
+
+        if ((transform.position.z)<= 1700f) { // If camera is in front of the pin
+
+            transform.position = ball.transform.position + offset;
+        }
+
+		
+	}
+}

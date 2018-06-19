076a53d720967182e281adf31e8ada3c5fb6e7ad
Komstuch <komstuchxbox@gmail.com>
2018-06-19 22:40:58 +0200

    Add Pin.cs code to detect if the Pin is standing

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    public float standingThreshold = 5f;
+
+	void Start () {
+
+	}
+	
+	void Update () {
+        print(name + " " + IsStanding());
+    }
+
+    public bool IsStanding(){
+        Vector3 rotationInEuler = transform.rotation.eulerAngles;
+
+        float tiltInX = Mathf.Abs(rotationInEuler.x);
+        float tiltInZ = Mathf.Abs(rotationInEuler.z);
+
+        if (tiltInX <= standingThreshold && tiltInZ <= standingThreshold) {
+            return true;
+        }
+        return false;
+    }
+}

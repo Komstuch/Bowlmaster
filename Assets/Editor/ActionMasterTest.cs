8d87654626eb6b3c086432fe9a50e57812f78ad4
Komstuch <komstuchxbox@gmail.com>
2018-06-24 12:23:43 +0200

    Test Driven Development introduction

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class ActionMasterTest {
+
+    private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+    private ActionMaster actionMaster = new ActionMaster();
+
+    [Test]
+    public void T00PassingTest(){
+        Assert.AreEqual(1, 1);
+    }
+
+    [Test]
+    public void T01OneStrikeReturnsEndTurn (){
+        Assert.AreEqual(endTurn, actionMaster.Bowl(10)); 
+    }
+}

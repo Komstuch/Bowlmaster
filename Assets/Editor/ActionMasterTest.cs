73073f877926a3af9ee896b3b2550c2bd3e3b493
Komstuch <komstuchxbox@gmail.com>
2018-06-26 17:51:56 +0200

    TDD Continue

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -6,8 +6,14 @@ using UnityEngine;
 [TestFixture]
 public class ActionMasterTest {
 
+    private ActionMaster actionMaster;
     private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
-    private ActionMaster actionMaster = new ActionMaster();
+    private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+
+    [SetUp] //It runs every time the test is run
+    public void Setup() {
+        actionMaster = new ActionMaster();
+    }
 
     [Test]
     public void T00PassingTest(){
@@ -15,7 +21,18 @@ public class ActionMasterTest {
     }
 
     [Test]
-    public void T01OneStrikeReturnsEndTurn (){
+    public void T01OneStrikeReturnsEndTurn () {
         Assert.AreEqual(endTurn, actionMaster.Bowl(10)); 
+    }
+
+    [Test]
+    public void T02Bowl8ReturnsTidy() {
+        Assert.AreEqual(tidy, actionMaster.Bowl(8));
+    }
+
+    [Test]
+    public void T03Bowl28SpareReturnsEndTurn() {
+        actionMaster.Bowl(2);
+        Assert.AreEqual(endTurn, actionMaster.Bowl(8));
     }
 }
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -6,17 +6,28 @@ public class ActionMaster{
 
     public enum Action {Tidy, Reset, EndTurn, EndGame}
 
+    //private int[] bowls = new int[21];
+    private int bowl = 1;
+
     public Action Bowl(int pins) {
 
-        //Other behaviour here
+        //Other behaviour here e.g. last frame
 
         if (pins < 0 || pins > 10) { throw new UnityException("Invalid Pins");}
 
         if (pins == 10) {
+            bowl += 2;
             return Action.EndTurn;
         }
 
-        //Other behaviour here
+        //Modulo 2 = rest form division by 2
+        if (bowl % 2 != 0) { //Mid frame or last frame  
+            bowl += 1;
+            return Action.Tidy;
+        } else if (bowl % 2 == 0){ //End of frame
+            bowl += 1;
+            return Action.EndTurn;
+        }
 
         throw new UnityException("Not sure what action to return");
     }

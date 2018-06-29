662e00e0cda71c53f8046a9121e77bc9d63f9f2d
Komstuch <komstuchxbox@gmail.com>
2018-06-29 11:31:34 +0200

    T013 Backup

--- Assets/Editor/ScoreMasterTest.cs
+++ Assets/Editor/ScoreMasterTest.cs
@@ -8,12 +8,14 @@ using UnityEngine;
 public class ScoreMasterTest{
 
     [Test]
-    public void T00PassingTest() {
+    public void T00PassingTest()
+    {
         Assert.AreEqual(1, 1);
     }
 
     [Test]
-    public void T01Bowl23() {
+    public void T01Bowl23()
+    {
         int[] rolls = { 2, 3 };
         int[] frames = { 5 };
         Assert.AreEqual(frames.ToList(), ScoreMaster.ScoreFrames(rolls.ToList()));
@@ -64,6 +66,62 @@ public class ScoreMasterTest{
     {
         int[] rolls = { 10, 1 };
         int[] frames = { };
+        Assert.AreEqual(frames.ToList(), ScoreMaster.ScoreFrames(rolls.ToList()));
+    }
+
+    [Test]
+    public void T08Bowl19()
+    {
+        int[] rolls = { 1, 9 };
+        int[] frames = { };
+        Assert.AreEqual(frames.ToList(), ScoreMaster.ScoreFrames(rolls.ToList()));
+    }
+
+    [Test]
+    public void T09Bowl123455()
+    {
+        int[] rolls = { 1, 2, 3, 4, 5, 5 };
+        int[] frames = { 3, 7 };
+        Assert.AreEqual(frames.ToList(), ScoreMaster.ScoreFrames(rolls.ToList()));
+    }
+
+    [Test]
+    public void T10SpareBonus()
+    {
+        int[] rolls = { 1, 2, 3, 5, 5, 5, 3, 3 };
+        int[] frames = { 3, 8, 13, 6 };
+        Assert.AreEqual(frames.ToList(), ScoreMaster.ScoreFrames(rolls.ToList()));
+    }
+
+    [Test]
+    public void T11SpareBonus2()
+    {
+        int[] rolls = { 1, 2, 3, 5, 5, 5, 3, 3, 7, 1, 9, 1, 6 };
+        int[] frames = { 3, 8, 13, 6, 8, 16 };
+        Assert.AreEqual(frames.ToList(), ScoreMaster.ScoreFrames(rolls.ToList()));
+    }
+
+    [Test]
+    public void T12StrikeBonus()
+    {
+        int[] rolls = { 10, 3, 4 };
+        int[] frames = { 17, 7 };
+        Assert.AreEqual(frames.ToList(), ScoreMaster.ScoreFrames(rolls.ToList()));
+    }
+
+    [Test]
+    public void T13StrikeBonus3()
+    {
+        int[] rolls = { 1, 2, 3, 4, 5, 4, 3, 2, 10, 1, 3, 3, 4 };
+        int[] frames = { 3, 7, 9, 5, 14, 4, 7 };
+        Assert.AreEqual(frames.ToList(), ScoreMaster.ScoreFrames(rolls.ToList()));
+    }
+
+    [Test]
+    public void T14MultiStrikes()
+    {
+        int[] rolls = { 10, 10, 2, 3 };
+        int[] frames = { 22, 15, 5 };
         Assert.AreEqual(frames.ToList(), ScoreMaster.ScoreFrames(rolls.ToList()));
     }
 
--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -27,17 +27,42 @@ public class ScoreMaster {
         //int frameNumber = 1;
         int rollsCount = 0;
         int frameScore = 0;
+        int StrikeBonus = 0;
+        int StrikeCounter = 0; 
+        int SpareBonus = 0;
 
         foreach (int roll in rolls)
         {
             rollsCount++;
-            frameScore += roll; 
-            if (rollsCount % 2 == 0) {
-                frameList.Add(frameScore);
-                frameScore = 0;
+
+            if (roll == 10 && (rollsCount % 2) == 1) {
+                StrikeBonus += roll;
+                StrikeCounter = 2;
+                rollsCount += 1;
+            } else {
+                frameScore += roll;
+                if(StrikeCounter > 0)
+                {
+                    StrikeBonus += roll;
+                    StrikeCounter--;
+                    if (StrikeCounter == 0) {
+                        frameList.Add(StrikeBonus);
+                    }
+                }
+                if (rollsCount % 2 == 0) {
+                    if (frameScore == 10) {
+                        SpareBonus = frameScore;
+                        frameScore = 0;
+                    }
+                    else {
+                        frameList.Add(frameScore);
+                        frameScore = 0;
+                    }
+                } else if (SpareBonus !=0) {
+                    frameList.Add(roll + SpareBonus);
+                    SpareBonus = 0;
+                }
             }
-
-
         }
         return frameList;
     }
@@ -45,3 +70,27 @@ public class ScoreMaster {
 
 
 }
+
+
+//public static List<int> ScoreFrames(List<int> rolls)
+//{
+//    List<int> frameList = new List<int>();
+
+//    //int frameNumber = 1;
+//    int rollsCount = 0;
+//    int frameScore = 0;
+
+//    foreach (int roll in rolls)
+//    {
+//        rollsCount++;
+//        frameScore += roll;
+//        if (rollsCount % 2 == 0)
+//        {
+//            frameList.Add(frameScore);
+//            frameScore = 0;
+//        }
+
+
+//    }
+//    return frameList;
+//}

456b64d5baaeff4350df1bef0605d199fedbce5c
Komstuch <komstuchxbox@gmail.com>
2018-06-29 09:38:06 +0200

    Backup #1 for ScoreMaster TDD

---
+++ Assets/Editor/ScoreMasterTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class ScoreMasterTest{
+
+    [Test]
+    public void T00PassingTest() {
+        Assert.AreEqual(1, 1);
+    }
+
+    [Test]
+    public void T01Bowl23() {
+        int[] rolls = { 2, 3 };
+        int[] frames = { 5 };
+        Assert.AreEqual(frames.ToList(), ScoreMaster.ScoreFrames(rolls.ToList()));
+    }
+
+}
--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class ScoreMaster {
 
+
+
+    //Returns a list of cumulative scores like a normal scorecard
+    public static List<int> ScoreCumulative(List<int> rolls)
+    {
+        List<int> cumulativeScore = new List<int>();
+        int runningTotal = 0;
+
+        foreach (int frameScore in ScoreFrames(rolls))
+        {
+            runningTotal += frameScore;
+            cumulativeScore.Add(runningTotal);
+        }
+        return cumulativeScore;
+    }
+
+    //Returns list of individual frame scores
     public static List<int> ScoreFrames (List<int> rolls) {
         List<int> frameList = new List<int>();
 
@@ -11,5 +28,7 @@ public class ScoreMaster {
 
         return frameList;
     }
+
+
 
 }
